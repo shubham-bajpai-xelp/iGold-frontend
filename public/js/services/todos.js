db148b0e014dc0d741afe082db3772d1665fa86b
Manish <manish@Manishs-MacBook-Pro.local>
2017-11-03 16:43:46 +0530

    create Angular structure and html file rendering

--- public/js/services/todos.js
+++ public/js/services/todos.js
@@ -5,13 +5,13 @@ angular.module('todoService', [])
 	.factory('Todos', ['$http',function($http) {
 		return {
 			get : function() {
-				return $http.get('/api/todos');
+				return $http.get('www.google.com');
 			},
-			create : function(todoData) {
-				return $http.post('/api/todos', todoData);
-			},
-			delete : function(id) {
-				return $http.delete('/api/todos/' + id);
-			}
+			// create : function(todoData) {
+			// 	return $http.post('/api/todos', todoData);
+			// },
+			// delete : function(id) {
+			// 	return $http.delete('/api/todos/' + id);
+			// }
 		}
 	}]);
--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-// set up ======================================================================
-var express = require('express');
-var app = express(); 						// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
-// configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
-
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
-
-// routes ======================================================================
-require('./app/routes.js')(app);
-
-// listen (start app with node server.js) ======================================
-app.listen(port);
-console.log("App listening on port " + port);
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const path = require('path');
+const port = process.env.PORT || 8080;
+app.use(express.static(`${__dirname}/public`));
+app.set('views', `${__dirname}/public/views`);
+app.use('/styles', express.static(path.join(__dirname, '/public/css/')));
+app.use('/img', express.static(path.join(__dirname, '/public/imag/')));
+app.use('/fonts', express.static(path.join(__dirname, '/public/fonts/')));
+app.engine('html', require('ejs').renderFile);
+require('./routes.js')(app);
+http.listen(port);
+process.on('uncaughtException', (err) => {
+    console.log(err.message);
+    console.error(err.stack);
+    process.exit(1);
+});
+module.exports = app;
