6ea8b58e854efc210b86954cef0707c5e2b1f6fc
Manish <shubham.bajpai@xelpmoc.in>
2017-11-07 13:21:06 +0530

    changes in the routes for kick start

--- routes.js
+++ routes.js
@@ -1,6 +1,23 @@
 module.exports = function(app) {
+    const headerDetails = {};
     app.get('/*', function(req, res,next) {
-        res.render('index.html')
+        var designatedPath = req.path;
+        headerDetails.protocol=req.protocol;
+        headerDetails.servers=headerDetails.protocol+'://'+req.hostname;
+        headerDetails.server=req.hostname;
+        headerDetails.page=designatedPath.replace('/','');
+        headerDetails.services='adminNew';
+        headerDetails.title=headerDetails.page;
+        headerDetails.controllerFile = '';
+        switch(designatedPath){
+            case '/jeweller':
+            case '/banker':
+                res.render(headerDetails.service+headerDetails.page+'.html',{headerDetail:headerDetails});
+            break;
+            case '/login':
+            res.render(headerDetails.service+headerDetails.page+'.html',{headerDetails:headerDetails});
+            break;
+        }
     });
   }
   
