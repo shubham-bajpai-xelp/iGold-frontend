763712f42c2b1336316c7ae4b524b9fb54f48a6e
Manish <shubham.bajpai@xelpmoc.in>
2017-11-08 18:46:59 +0530

    signin procedure is completed for the users

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,6 +1,7 @@
 app.controller("loginControl", function(
   $scope,
   $location,
+  $cookieStore,
   userSignIn,
   dataFactory
 ) {
@@ -15,21 +16,27 @@ app.controller("loginControl", function(
       var dt = {};
       dt.email = $scope.userDetail.username;
       dt.password = $scope.userDetail.password;
-      var url = "http://192.168.2.154:3000/login";
+      var url = "http://localhost:3000/login";
       dataFactory.getPostData(url, dt).then(function(res) {
+        var status = res.data.status;
+        if(status==200){
+            var response = eval('('+res.data.body+')');
+            console.log(response);
+            if (response.userType == 0 && response.userId) {
+                $cookieStore.put('visitorId',response.userId);
+                $cookieStore.put('visitorToken',response.token);
+                window.location.href = "bankview";
+                //  $rootScope.loggedIn=true;
+                //  $location.path('/bankview');
+            }
+            else if (response.userType == 1 && response.userId) {
+                window.location.href = "jewelview";
+            }
+            else {
+                alert('wrong stuff');
+            }
 
-        res = res;
-        // if (uname == 'admin' && pswrd == 'admin') {
-        //     window.location.href = "bankview";
-        //     //  $rootScope.loggedIn=true;
-        //     //  $location.path('/bankview');
-        // }
-        // else if (uname == 'bajpai' && pswrd == 'bajpai') {
-        //     window.location.href = "jewelview";
-        // }
-        // else {
-        //     alert('wrong stuff');
-        // }
+        }
       });
     }
   };
