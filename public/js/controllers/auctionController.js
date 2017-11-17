4e1152213a92ed509ff31d544b841c36e7c168e6
neoxelpmoc <rajeshbankar80@gmail.com>
2017-11-17 18:46:10 +0530

    creat auction page ng-model value issue solved

--- public/js/controllers/auctionController.js
+++ public/js/controllers/auctionController.js
@@ -1,10 +1,10 @@
-app.directive("file", function() {
+app.directive("file", function () {
   return {
     scope: {
       file: "="
     },
-    link: function(scope, el, attrs) {
-      el.bind("change", function(event) {
+    link: function (scope, el, attrs) {
+      el.bind("change", function (event) {
         var file = event.target.files[0];
         scope.file = file ? file : undefined;
         scope.$apply();
@@ -12,7 +12,7 @@ app.directive("file", function() {
     }
   };
 });
-app.controller("auctionControl", function(
+app.controller("auctionControl", function (
   $scope,
   $http,
   postAuctionForm,
@@ -20,9 +20,9 @@ app.controller("auctionControl", function(
   dataFactory,
   $q
 ) {
-  $scope.pageName=page;
+  $scope.pageName = page;
   $scope.auctionData = {};
-  $scope.postAuctionDetails = function(id) {
+  $scope.postAuctionDetails = function (id) {
     var formObj = {
       bankName: $scope.auctionData.bankName,
       bankBranch: $scope.auctionData.bankBranch,
@@ -37,6 +37,7 @@ app.controller("auctionControl", function(
       bankEMDLastDate: $scope.auctionData.bankEMDLastDate,
       auctionPacketFile: $scope.auctionData.auctionPacketFile
     };
+    console.log(formObj);
     $scope.checkValid = postAuctionForm.validateAuctionForm(formObj);
     if ($scope.checkValid == true) {
       var url = "http://localhost:3000/banker/postAuction";
@@ -49,14 +50,14 @@ app.controller("auctionControl", function(
       dt.bankBranch = $scope.auctionData.bankBranch;
       var bankId = $cookies.get('visitorId');
       dt.bankId = bankId.split('"')[1];
-      dt.bankClass= "axis_bank";
-      dt.address=$scope.auctionData.bankAddress;
-      dt.city= $scope.auctionData.bankCity;
-      dt.auctionDate= $scope.auctionData.auctionDate;
-      dt.auctionTime= $scope.auctionData.auctionTime;
-      dt.auctionEMDAmount=$scope.auctionData.auctionEMDAmount;
-      dt.EMDAccountNumber= $scope.auctionData.accountNumber;
-      dt.description= "";
+      dt.bankClass = "axis_bank";
+      dt.address = $scope.auctionData.bankAddress;
+      dt.city = $scope.auctionData.bankCity;
+      dt.auctionDate = $scope.auctionData.auctionDate;
+      dt.auctionTime = $scope.auctionData.auctionTime;
+      dt.auctionEMDAmount = $scope.auctionData.auctionEMDAmount;
+      dt.EMDAccountNumber = $scope.auctionData.accountNumber;
+      dt.description = "";
       dt.EMDAccountNo = $scope.auctionData.auctionNumber;
       dt.pinCode = $scope.auctionData.bankPincode;
       dt.state = $scope.auctionData.bankState;
@@ -67,11 +68,73 @@ app.controller("auctionControl", function(
         headers: { "Content-Type": undefined },
         transformRequest: angular.identity
       };
-      dataFactory.postFormData(url, fd, config).then(function(resp) {
+      dataFactory.postFormData(url, fd, config).then(function (resp) {
         var response = JSON.parse(resp.data.body);
         console.log(response);
       });
       $('.button').addClass('addPointer loaderNxt');
     }
   };
+});
+app.directive("showNotifications", function () {
+  return {
+    restrict: "A",
+    link: function ($scope, elem, attrs) {
+      $(elem).click(function () {
+        var tm = elem[0].innerHTML
+        console.log(tm);
+        $scope.auctionData.bankCity = tm;
+        $(this).parent('ul').prev('.dropdown_arrow').prev('.col100').find('label').addClass('labelActive');
+        $(this).parent('ul').slideUp('swing');
+        $(this).parent('ul').prev('.dropdown_arrow').prev('.col100').find('input[type=text]').val($(this).text());
+        $(this).parent('ul').prev('.dropdown_arrow').removeClass('rot180');
+      });
+    }
+  }
+});
+app.directive("showState", function () {
+  return {
+    restrict: "A",
+    link: function ($scope, elem, attrs) {
+      $(elem).click(function () {
+        var mk = elem[0].innerHTML
+        console.log(mk);
+        $scope.auctionData.bankState = mk;
+        $(this).parent('ul').prev('.dropdown_arrow').prev('.col100').find('label').addClass('labelActive');
+        $(this).parent('ul').slideUp('swing');
+        $(this).parent('ul').prev('.dropdown_arrow').prev('.col100').find('input[type=text]').val($(this).text());
+        $(this).parent('ul').prev('.dropdown_arrow').removeClass('rot180');
+      });
+    }
+  }
+});
+app.directive("datepicker", function () {
+  function link(scope, element, attrs, controller) {
+    $(element).datepicker({
+      onSelect: function (dt) {
+        scope.$apply(function () {
+          controller.$setViewValue(dt);
+        });
+        if ($(this).attr('id') == 'tm') {
+          $('#tm').next('.label').addClass('labelActive');
+        }
+        else if ($(this).attr('id') == 'lb') {
+          $('#lb').next('.label').addClass('labelActive');
+        }
+      },
+      dateFormat: "dd/mm/yy"
+    });
+  } return { require: 'ngModel', link: link };
+});
+app.directive("timepicker", function () {
+  function link(scope, element, attrs, controller) {
+    $(element).timepicker({
+      onSelect: function (dt) {
+        scope.$apply(function () {
+          controller.$setViewValue(dt);
+        });
+        $('#vm').next('.label').addClass('labelActive');
+      },
+    });
+  } return { require: 'ngModel', link: link };
 });
--- public/js/jquery/creatAuction.js
+++ public/js/jquery/creatAuction.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-      $('.timepicker').timepicker({
-      onSelect:function (date){
-        $('#vm').next('.label').addClass('labelActive');
-      }
-      });  
-      $( ".datepicker" ).datepicker({
-            onSelect: function (date) {
-           if($(this).attr('id')=='tm'){
-            $('#tm').next('.label').addClass('labelActive');
-           }
-           else if($(this).attr('id')=='lb'){
-            $('#lb').next('.label').addClass('labelActive');    
-           }
-      }     
-      });
-});
+// $(document).ready(function(){
+//       $('.timepicker').timepicker({
+//       onSelect:function (date){
+//         $('#vm').next('.label').addClass('labelActive');
+//       }
+//       });  
+//       // $( ".datepicker" ).datepicker({
+//       //       onSelect: function (date) {
+//       //      if($(this).attr('id')=='tm'){
+//       //       $('#tm').next('.label').addClass('labelActive');
+//       //      }
+//       //      else if($(this).attr('id')=='lb'){
+//       //       $('#lb').next('.label').addClass('labelActive');    
+//       //      }
+//       // }     
+//       // });
+// });
 
 
